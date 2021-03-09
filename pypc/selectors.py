63d3cdc258268aa35ff510fa833c88ed7d31c632
DESKTOP-75401JP\mclea <mcleancolinm@gmail.com>
2021-03-08 22:04:38 -0600

    Impliment selectors and tests for them

--- pypc/selectors.py
+++ pypc/selectors.py
@@ -1,2 +1,24 @@
 from .nand import nand
 
+
+def mux(selector: bool, a: bool, b: bool) -> bool:
+    """Multiplexer: returns a if selector is True, otherwise returns b."""
+    not_selector = nand(selector, selector)
+    nand_s_a = nand(selector, a)
+    nand_not_s_b = nand(not_selector, b)
+    return nand(nand_s_a, nand_not_s_b)
+
+
+def demux(selector: bool, in_: bool) -> (bool, bool):
+    """Demultiplexer: Returns pairs of bools (a, b) based on selector.
+    
+    If selector is True, a will equal the input.
+    If selector is False, b will equal the input.
+    
+    The non-selected output will always be false."""
+    not_selector = nand(selector, selector)
+    nand_not_s_in = nand(not_selector, in_)
+    nand_s_in = nand(selector, in_)
+    a = nand(nand_s_in, nand_s_in)
+    b = nand(nand_not_s_in, nand_not_s_in)
+    return a, b
---
+++ pypc/tests/selector_tests.py
@@ -0,0 +1,40 @@
+﻿from pypc.selectors import mux, demux
+import unittest
+
+
+def run_tests(test_class):
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
+
+
+class TestArithmetic(unittest.TestCase):
+    def setUp(self):
+        self.mux_pairs = [
+            ((False, False, False), False),
+            ((False, True, False), False),
+            ((False, False, True), True),
+            ((False, True, True), True),
+            ((True, False, False), False),
+            ((True, False, True), False),
+            ((True, True, False), True),
+            ((True, True, True), True),
+        ]
+
+        self.demux_pairs = [
+            ((False, False), (False, False)),
+            ((False, True), (False, True)),
+            ((True, False), (False, False)),
+            ((True, True), (True, False)),
+        ]
+
+    def test_mux(self):
+        for (sel, a, b), out in self.mux_pairs:
+            self.assertEqual(mux(sel, a, b), out)
+
+    def test_demux(self):
+        for (sel, in_), (a, b) in self.demux_pairs:
+            self.assertEqual(demux(sel, in_), (a, b))
+
+
+run_tests(TestArithmetic)
