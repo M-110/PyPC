42eb359e5ebd89b940b334202dd32a3e65943d3e
DESKTOP-75401JP\mclea <mcleancolinm@gmail.com>
2021-03-31 15:16:42 -0500

    Create a function simulating the CPU

--- pypc/i_arithmetic_logic_unit/alu.py
+++ pypc/i_arithmetic_logic_unit/alu.py
@@ -1,4 +1,6 @@
-﻿from pypc.pypc_typing import Bool16
+﻿from typing import Tuple
+
+from pypc.pypc_typing import Bool16
 from pypc.e_16_bit_logic.selectors_16 import mux_16
 from pypc.e_16_bit_logic.core_logic_gates_16 import not_16
 from pypc.f_16_bit_arithmetic.arithmetic_16 import adder_16
@@ -7,11 +9,15 @@ from pypc.i_arithmetic_logic_unit.unary_alu import unary_alu
 
 
 def alu(zero_x: bool, negate_x: bool, zero_y: bool, negate_y: bool,
-        add_x_y: bool, negate_output: bool, x: Bool16, y: Bool16) -> Bool16:
+        add_x_y: bool, negate_output: bool, x: Bool16, y: Bool16)\
+        -> Tuple[Bool16, bool, bool]:
     unary_x = unary_alu(zero_x, negate_x, x)
     unary_y = unary_alu(zero_y, negate_y, y)
     x_add_y = adder_16(unary_x, unary_y)
     x_and_y = and_16(unary_x, unary_y)
     mux_add_and = mux_16(add_x_y, x_add_y, x_and_y)
     negated_out = not_16(mux_add_and)
-    return mux_16(negate_output, negated_out, mux_add_and)
+    output = mux_16(negate_output, negated_out, mux_add_and)
+    zr = False
+    ng = False
+    return output, zr, ng
--- pypc/j_computer/cpu.py
+++ pypc/j_computer/cpu.py
@@ -1,6 +1,11 @@
-﻿from pypc.b_basic_logic.core_logic_gates import or_, and_, not_
+﻿from typing import Tuple
+
+from pypc.b_basic_logic.core_logic_gates import or_, and_, not_
 from pypc.e_16_bit_logic.selectors_16 import mux_16
-from pypc.i_arithmetic_logic_unit import alu
+from pypc.g_16_bit_memory.program_counter import pc_factory
+from pypc.g_16_bit_memory.register import register_16_bit_factory
+from pypc.i_arithmetic_logic_unit.alu import alu
+from pypc.pypc_typing import Bool16, Bool15
 
 """
 ### A Instruction format: VVVVVVVVVVVVVVV 0
@@ -34,3 +39,105 @@ C instructions begin with 1:
         Always just 111
     
 """
+
+a_register = register_16_bit_factory()
+d_register = register_16_bit_factory()
+pc = pc_factory()
+
+
+def cpu(m_in: Bool16, instructions: Bool16, reset: bool) \
+        -> Tuple[Bool16, bool, Bool15, Bool15]:
+    """Simulation of the CPU unit.
+    
+    Args:
+        m_in: Value of the M RAM input.
+        instructions: 16 bit instructions for the CPU to follow.
+        reset: If True, program will be restarted.
+        
+    Returns:
+        (a, b, c, d) where
+        a: M output value.
+        b: Write to M instruction.
+        c: Address of M
+        d: address of the next instruction
+    """
+
+    alu_out = Bool16
+
+    # If first instruction digit is 1, then it is a C instruction.
+    a_instruction = not_(instructions[15])
+    c_instruction = not_(a_instruction)
+
+    # # # # # # # # # # # # # # Register A
+    # instructions[5] represents whether A is the destination
+    alu_to_a = and_(a=c_instruction, b=instructions[5])
+
+    # If it's a C instruction and the destination is A then send the ALU output
+    # to the A register. Otherwise send the instruction to Register A.
+    register_a_input = mux_16(alu_to_a, instructions, alu_out)  # TODO: Add sequential logic.
+
+    # load_a is True if it is an A instruction or the C instruction has A as
+    # the destination.
+    load_a = or_(a_instruction, alu_to_a)
+    a_to_pc = a_register(register_a_input, load_a)
+    register_a_out = a_to_pc
+    m_address = a_to_pc[0:15]  # Returned as output ->
+
+    # if instructions[12] is True M should be used instead of the A register
+    am_mux_out = mux_16(instructions[12], register_a_out, m_in)
+
+    # # # # # # # # # # # # # # Register D
+    # If instructions[4] is True the D register is the destination (if it is a
+    # c instruction).
+    load_d = and_(a=c_instruction, b=instructions[4])
+    register_d_out = d_register(load_d, alu_out)  # TODO: Fix d register?
+
+    # # # # # # # # # # # # # # ALU
+    # Send the register_d_out into the ALU
+    alu_out, zero_out, neg_out = alu(x=register_d_out,
+                                     y=am_mux_out,
+                                     zero_x=instructions[11],
+                                     negate_x=instructions[10],
+                                     zero_y=instructions[9],
+                                     negate_y=instructions[8],
+                                     add_x_y=instructions[7],
+                                     negate_output=instructions[6])
+    
+    m_out = alu_out  # Returned as output ->
+    
+    # If it is a C instruction and the destination is M, write to M
+    write_m = and_(c_instruction, instructions[3])  # Returned as output ->
+    
+    # # # # # # # # # # # # # # PC / JUMP conditionals
+    # Jump if equal to zero
+    jeq = and_(zero_out, instructions[1])
+    # Jump if less than zero
+    jlt = and_(neg_out, instructions[2])
+    
+    # Zero or negative
+    zero_or_neg = or_(zero_out, neg_out)
+    # Positive
+    positive = not_(zero_or_neg)
+    
+    # Jump if greater than zero
+    jgt = and_(positive, instructions[0])
+    # Jump if less than or equal to zero
+    jle = or_(jeq, jlt)
+    
+    # If any conditions are met, ready the jump to A
+    # (less than or equal to and greater than cover all possible conditions
+    # so if either is True, there must be a jump).
+    jump_to_a = or_(jle, jgt)
+    
+    # Only jump if it a C instruction though!
+    load_pc = and_(c_instruction, jump_to_a)
+    # If not loading an address, increase the PC
+    inc_pc = not_(load_pc)
+    
+    # If any conditions were met then the PC will load the address,
+    # otherwise the PC will just increase the address to the program's next line.
+    pc_out = pc(a_to_pc, load_pc, inc_pc, reset)[:15] # returned as output
+    
+    return m_out, write_m, m_address, pc_out
+    
+
