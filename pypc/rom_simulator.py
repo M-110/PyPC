028e1af91977870fac66915fe11e8a7c193aae63
DESKTOP-75401JP\mclea <mcleancolinm@gmail.com>
2021-05-24 22:53:17 -0500

    Add documentation to ROM simulator class

--- pypc/rom_simulator.py
+++ pypc/rom_simulator.py
@@ -2,6 +2,7 @@
 
 
 class ROM:
+    """Class for simulating hard-disk storage."""
     def __init__(self, file=None):
         self.file = file
         self.data = None
@@ -9,10 +10,12 @@ class ROM:
             self.read_file()
         
     def read_file(self):
+        """Read a binary file and assign each line of the file an address."""
         with open(self.file, encoding='utf-8-sig') as f:
             self.data = [x[::-1] for x in f.read().split('\n') if x]
         
     def read_address(self, address):
+        """Read from the bool16 address location."""
         address_index = bool16_to_int(address)
         try:
             out = tuple(True if c == '1' else False for c in self.data[address_index])
